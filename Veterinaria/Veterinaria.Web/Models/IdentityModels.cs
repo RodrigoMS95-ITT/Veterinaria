a743fd24990f63e2aaa367e1364564bd107bc364
RodrigoMS95-ITT <rodrigo.murguia17@tectijuana.edu.mx>
2020-05-23 17:34:52 -0700

    update video 2

--- Veterinaria/Veterinaria.Web/Models/IdentityModels.cs
+++ Veterinaria/Veterinaria.Web/Models/IdentityModels.cs
@@ -1,19 +1,34 @@
-﻿using System.Data.Entity;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 
+
 namespace Veterinaria.Web.Models
 {
-    // Para agregar datos de perfil del usuario, agregue más propiedades a su clase ApplicationUser. Visite https://go.microsoft.com/fwlink/?LinkID=317594 para obtener más información.
+    // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        [Display(Name = "Nombre")]
+        [MaxLength(50)]
+        public string FirstName { get; set; }
+        [Display(Name = "Apellidos")]
+        [MaxLength(50)]
+        public string Lastname { get; set; }
+        [MaxLength(400)]
+        [Display(Name ="Dirección")]
+        public string Address { get; set; }
+        [Display(Name ="Picture")]
+        public string ImgUrl { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
-            // Tenga en cuenta que el valor de authenticationType debe coincidir con el definido en CookieAuthenticationOptions.AuthenticationType
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
             var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Agregar aquí notificaciones personalizadas de usuario
+            // Add custom user claims here
+            
             return userIdentity;
         }
     }
@@ -29,5 +44,13 @@ namespace Veterinaria.Web.Models
         {
             return new ApplicationDbContext();
         }
+
+        public DbSet<Owner> Owners { get; set; }
+        public DbSet<Pet> Pets { get; set; }
+        public DbSet<Consult> Consults { get; set; }
+        public DbSet<Veterinary> Veterinaries { get; set; }
+        public DbSet<History> Histories { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+
     }
 }
